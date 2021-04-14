5a959e8ad35e1eb6ebd12b08ff71474cb982a090
yuri <yurirdn@ufrn.edu.br>
2021-04-14 20:26:54 -0300

    feat: implementa leitura dos frames dos vídeos e exibe com opencv

--- deteccao_objeto_open_cv.py
+++ deteccao_objeto_open_cv.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[5]:
 
 
-print('Hello World')
+import cv2, numpy, os
+
+
+# In[11]:
+
+
+pathFrames = r'\assets\caxorrimFrames'
+#pathFrames = r'\assets\dogDriftFrames'
+#pathFrames = r'\assets\gatoPiscinaFrames'
+diretorioFrames = os.getcwd() + pathFrames
+
+
+# In[12]:
+
+
+for nomeframe in os.listdir(diretorioFrames): 
+    caminhoFrame = os.path.join(diretorioFrames, nomeframe)
+    frame = cv2.imread(caminhoFrame)
+    frame = cv2.resize(frame, dsize=(600, 400))
+    cv2.imshow('Teste', frame)
+    cv2.waitKey(30)
+    
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
