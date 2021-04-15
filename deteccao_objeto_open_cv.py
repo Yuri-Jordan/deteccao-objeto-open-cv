b9bf4571a1be1cd8bff68a6d7c43725ef3173ad9
Yuri Jordan <yurijrdn@gmail.com>
2021-04-15 12:07:32 -0300

    feat: remove pixels de background dos frames usando o modelo Gaussiano do opencv

--- deteccao_objeto_open_cv.py
+++ deteccao_objeto_open_cv.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[1]:
 
 
 import cv2, numpy, os
 
 
-# In[11]:
+# In[14]:
 
 
 pathFrames = r'\assets\caxorrimFrames'
@@ -16,14 +16,36 @@ pathFrames = r'\assets\caxorrimFrames'
 diretorioFrames = os.getcwd() + pathFrames
 
 
-# In[12]:
+# In[15]:
+
+
+foregroundModel = cv2.createBackgroundSubtractorMOG2()
+
+
+# In[16]:
+
+
+def aplicar_mascara(frame):
+    foregroundMask = foregroundModel.apply(frame)
+    matrizVazia = numpy.zeros(frame.shape, numpy.uint8)
+    matrizVazia[:,:,0], matrizVazia[:,:,1], matrizVazia[:,:,2] = foregroundMask, foregroundMask, foregroundMask
+    frameConcat = numpy.hstack((frame, matrizVazia))
+    return frameConcat
+
+
+# In[17]:
 
 
 for nomeframe in os.listdir(diretorioFrames): 
+    
     caminhoFrame = os.path.join(diretorioFrames, nomeframe)
+    
     frame = cv2.imread(caminhoFrame)
     frame = cv2.resize(frame, dsize=(600, 400))
-    cv2.imshow('Teste', frame)
+    frameConcat = aplicar_mascara(frame)
+    
+    
+    cv2.imshow('Teste', frameConcat)
     cv2.waitKey(30)
     
 cv2.destroyAllWindows()
