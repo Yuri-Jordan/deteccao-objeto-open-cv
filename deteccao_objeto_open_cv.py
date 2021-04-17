301d02931bb7040906c5150123ef8efa572afb11
Yuri Jordan <yurijrdn@gmail.com>
2021-04-16 21:47:12 -0300

    feat: reduz ruídos da imagem de foreground

--- deteccao_objeto_open_cv.py
+++ deteccao_objeto_open_cv.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[16]:
 
 
 import cv2, numpy, os
 
 
-# In[14]:
+# In[59]:
 
 
 pathFrames = r'\assets\caxorrimFrames'
@@ -16,24 +16,33 @@ pathFrames = r'\assets\caxorrimFrames'
 diretorioFrames = os.getcwd() + pathFrames
 
 
-# In[15]:
+# In[60]:
 
 
 foregroundModel = cv2.createBackgroundSubtractorMOG2()
 
 
-# In[16]:
+# In[61]:
+
+
+def reduzir_ruidos(foregroundMask):
+    morf = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    return cv2.morphologyEx(numpy.float32(foregroundMask), cv2.MORPH_OPEN, morf)
+
+
+# In[62]:
 
 
 def aplicar_mascara(frame):
     foregroundMask = foregroundModel.apply(frame)
+    foregroundMask = reduzir_ruidos(foregroundMask)
     matrizVazia = numpy.zeros(frame.shape, numpy.uint8)
     matrizVazia[:,:,0], matrizVazia[:,:,1], matrizVazia[:,:,2] = foregroundMask, foregroundMask, foregroundMask
     frameConcat = numpy.hstack((frame, matrizVazia))
     return frameConcat
 
 
-# In[17]:
+# In[ ]:
 
 
 for nomeframe in os.listdir(diretorioFrames): 
@@ -46,15 +55,9 @@ for nomeframe in os.listdir(diretorioFrames):
     
     
     cv2.imshow('Teste', frameConcat)
-    cv2.waitKey(30)
+    cv2.waitKey(20)
     
 cv2.destroyAllWindows()
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
