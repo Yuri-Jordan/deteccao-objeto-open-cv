fe2e2121d60856125486f8d8a85188f1bc9b7d9a
Yuri Jordan <yurijrdn@gmail.com>
2021-04-17 22:18:16 -0300

    feat: define limiar do tamanho dos componentes da imagem que devem ser considerados como modificação do background

--- deteccao_objeto_open_cv.py
+++ deteccao_objeto_open_cv.py
@@ -7,7 +7,7 @@
 import cv2, numpy, os
 
 
-# In[59]:
+# In[69]:
 
 
 pathFrames = r'\assets\caxorrimFrames'
@@ -16,13 +16,13 @@ pathFrames = r'\assets\caxorrimFrames'
 diretorioFrames = os.getcwd() + pathFrames
 
 
-# In[60]:
+# In[70]:
 
 
 foregroundModel = cv2.createBackgroundSubtractorMOG2()
 
 
-# In[61]:
+# In[71]:
 
 
 def reduzir_ruidos(foregroundMask):
@@ -30,19 +30,39 @@ def reduzir_ruidos(foregroundMask):
     return cv2.morphologyEx(numpy.float32(foregroundMask), cv2.MORPH_OPEN, morf)
 
 
-# In[62]:
+# In[75]:
+
+
+def manter_objetos_significantes(foregroundMask):
+    limiar = 100
+    a, componentes_conectados = cv2.connectedComponents(numpy.array(foregroundMask > 0, numpy.uint8))
+    imagemForeground = numpy.zeros(componentes_conectados.shape)<0
+    componentes_unicos = numpy.unique(componentes_conectados.flatten())
+    
+    for componente in componentes_unicos:
+        if componente == 0: #significa que o pixel é de background
+            continue
+        else:
+            componente_conectado = componentes_conectados == componente
+            if numpy.sum(componente_conectado) > limiar:
+                imagemForeground = imagemForeground | componente_conectado
+    return numpy.uint8(255*imagemForeground)
+
+
+# In[73]:
 
 
 def aplicar_mascara(frame):
     foregroundMask = foregroundModel.apply(frame)
     foregroundMask = reduzir_ruidos(foregroundMask)
+    foregroundMask = manter_objetos_significantes(foregroundMask)
     matrizVazia = numpy.zeros(frame.shape, numpy.uint8)
     matrizVazia[:,:,0], matrizVazia[:,:,1], matrizVazia[:,:,2] = foregroundMask, foregroundMask, foregroundMask
     frameConcat = numpy.hstack((frame, matrizVazia))
     return frameConcat
 
 
-# In[ ]:
+# In[74]:
 
 
 for nomeframe in os.listdir(diretorioFrames): 
@@ -58,6 +78,12 @@ for nomeframe in os.listdir(diretorioFrames):
     cv2.waitKey(20)
     
 cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
